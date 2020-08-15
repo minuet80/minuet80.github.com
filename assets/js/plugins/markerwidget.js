1460173285ded5222c26d5a54f88009cbb55e25f
minuet80 <minuet80@gmail.com>
2020-08-15 13:07:11 +0900

    skdjfskldfjl

---
+++ assets/js/plugins/markerwidget.js
@@ -0,0 +1,12 @@
+(function(d, h, m){
+    var js, fjs = d.getElementsByTagName(h)[0];
+    if (d.getElementById(m)){return;}
+    js = d.createElement(h); js.id = m;
+    js.onload = function(){
+        window.makerWidgetComInit({
+        position: "left",          
+        widget: "nikrmszj14o0jbdu-9sttjzyemqjyhrck-acukp1tubknjpcem"                
+    })};
+    js.src = "https://makerwidget.com/js/embed.js";
+    fjs.parentNode.insertBefore(js, fjs)
+}(document, "script", "dhm"))
