939bb5876c09a39f01b2a0889ef5153da9f19e1e
minuet80 <minuet80@gmail.com>
2021-01-09 11:19:52 +0900

    test

--- assets/js/custom/custom.js
+++ assets/js/custom/custom.js
@@ -34,7 +34,90 @@ String.prototype.rpad = function(padLen, padStr) {
     return str;
 };
 
+var dates = {
+    convert:function(d) {
+        // Converts the date in d to a date-object. The input can be:
+        //   a date object: returned without modification
+        //  an array      : Interpreted as [year,month,day]. NOTE: month is 0-11.
+        //   a number     : Interpreted as number of milliseconds
+        //                  since 1 Jan 1970 (a timestamp) 
+        //   a string     : Any format supported by the javascript engine, like
+        //                  "YYYY/MM/DD", "MM/DD/YYYY", "Jan 31 2009" etc.
+        //  an object     : Interpreted as an object with year, month and date
+        //                  attributes.  **NOTE** month is 0-11.
+        return (
+            d.constructor === Date ? d :
+            d.constructor === Array ? new Date(d[0],d[1],d[2]) :
+            d.constructor === Number ? new Date(d) :
+            d.constructor === String ? new Date(d) :
+            typeof d === "object" ? new Date(d.year,d.month,d.date) :
+            NaN
+        );
+    },
+    compare:function(a,b) {
+        // Compare two dates (could be of any type supported by the convert
+        // function above) and returns:
+        //  -1 : if a < b
+        //   0 : if a = b
+        //   1 : if a > b
+        // NaN : if a or b is an illegal date
+        // NOTE: The code inside isFinite does an assignment (=).
+        return (
+            isFinite(a=this.convert(a).valueOf()) &&
+            isFinite(b=this.convert(b).valueOf()) ?
+            (a>b)-(a<b) :
+            NaN
+        );
+    },
+    inRange:function(d,start,end) {
+        // Checks if date in d is between dates in start and end.
+        // Returns a boolean or NaN:
+        //    true  : if d is between start and end (inclusive)
+        //    false : if d is before start or after end
+        //    NaN   : if one or more of the dates is illegal.
+        // NOTE: The code inside isFinite does an assignment (=).
+       return (
+            isFinite(d=this.convert(d).valueOf()) &&
+            isFinite(start=this.convert(start).valueOf()) &&
+            isFinite(end=this.convert(end).valueOf()) ?
+            start <= d && d <= end :
+            NaN
+        );
+    },
+    dday:function(start,end) {
+        var distance = end - start;//디데이에서 현재까지 뺀다.
+    
+        var d = Math.floor(distance / (1000 * 60 * 60 * 24));//일
+    
+        var h = Math.floor((distance / (1000*60*60)) % 24);//시간
+        var m = Math.floor((distance / (1000*60)) % 60);//분
+        var s = Math.floor((distance / 1000) % 60);//초
+
+        if (distance <= 0) {//당일넘어섰을때, dday로 변경
+            return d;
+        } else {
+            h = (h < 10 ? '0' + h : h);
+            m = (m < 10 ? '0' + m : m);
+            s = (s < 10 ? '0' + s : s);
+            return d + ' ' + (h + ':' + m + ':' + s);
+        }
+    }
+}
+
 $(document).ready(function() {
+
+    setInterval(function () {
+        var strtDay = new Date($('#dday1').find('span').data('day'));
+        var endDay = new Date();
+        var result = dates.dday(strtDay, endDay);
+        $('#dday1').find('span').html('😢' + result);
+    }, 1000);
+    setInterval(function () {
+        var strtDay = new Date($('#dday2').find('span').data('day'));
+        var endDay = new Date();
+        var result = dates.dday(strtDay, endDay);
+        $('#dday2').find('span').html('👰' + result);
+    }, 1000);
 
     var i = 0;
     var j = 0;
