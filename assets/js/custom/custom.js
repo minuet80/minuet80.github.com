a8454ee86dbd7ec30fd420d498801109bd7ce0da
minuet80 <minuet80@gmail.com>
2021-01-10 22:30:21 +0900

    test

--- assets/js/custom/custom.js
+++ assets/js/custom/custom.js
@@ -99,7 +99,7 @@ var dates = {
             h = (h < 10 ? '0' + h : h);
             m = (m < 10 ? '0' + m : m);
             s = (s < 10 ? '0' + s : s);
-            return d.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + ' ' + (h + ':' + m + ':' + s);
+            return d.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + ' (' + (h + ':' + m + ':' + s) + ')';
         }
     }
 }
